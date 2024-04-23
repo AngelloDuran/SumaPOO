95f2af0597b9f326804b70211020239fb421904c
AngelloDuran <duea020109hdfrsna5@gmail.com>
2024-04-23 00:51:03 -0600

    SumaPOO

--- MainSuma.java
+++ MainSuma.java
@@ -1,13 +1,13 @@
 import java.util.Scanner;
 
-public class MainResta {
+public class MainSuma {
     public static void main(String[] args) {
         Scanner ing = new Scanner(System.in);
         System.out.println("Ingresa el primero numero");
         int a = ing.nextInt();
         System.out.println("Ingrese el segundo numero");
         int b = ing.nextInt();
-        int r = a - b;
+        int r = a + b;
         System.out.println("El resultado es:" + r);
     }
 
